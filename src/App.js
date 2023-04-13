16290346a557076a87bc7fca98cac6818fa944f1
cmacspeedo <cmacspeedo101@gmail.com>
2023-04-13 17:23:10 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Valuable information I shouldn't see</h3> : ""}
+    </div>
+  );
+}
+
+export default App;
+
+// import React, { useCallback } from "react";
+// import { useDispatch } from "react-redux";
+
+// export const CounterComponent = ({ value }) => {
+//   const dispatch = useDispatch();
+//   const increaseCounter = useCallback(
+//     () => dispatch({ type: "increase-counter" }),
+//     []
+//   );
+//   return (
+//     <div>
+//       <span>{value}</span>
+//       <button onClick={increaseCounter}>Increase counter</button>
+//     </div>
+//   );
+// };
